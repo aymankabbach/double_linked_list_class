613c80984b8bccbc93ec792c3c98f2d2bdeee1d7
ayman <kabbachayman95@gmail.com>
2023-02-20 23:46:37 +0100

    add function insertAtBeginning

--- clsDblLinkedListClass.h
+++ clsDblLinkedListClass.h
@@ -6,6 +6,8 @@ using namespace std;
 template <class T>
 class clsDblLinkedList
 {
+protected:
+    int _size=0;
 public:
     class Node
     { 
@@ -16,4 +18,18 @@ public:
         
     };
     Node* head = NULL;
+    void InsertAtBeginning(T value)
+    {
+        Node* newNode= new Node();
+        newNode->value=value;
+        newNode->next=head;
+        newNode->prev=NULL;
+
+        if (head!=NULL)
+        {
+            head->prev=newNode;
+        }
+        head=newNode;
+        _size++;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main()
 {
     clsDblLinkedList <int> MyDblLinkedList;
+    MyDblLinkedList.InsertAtBeginning(5);
+    MyDblLinkedList.InsertAtBeginning(4);
+    MyDblLinkedList.InsertAtBeginning(3);
+    MyDblLinkedList.InsertAtBeginning(2);
+    MyDblLinkedList.InsertAtBeginning(1);
     system("pause");
     return 0;
 }
